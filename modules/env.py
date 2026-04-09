63825cc2351f3ea7bf7a545deda1b447089a02d8
Fuhito Suguri <bitwalk@gmail.com>
2026-04-09 10:55:03 +0900

    update

--- modules/env.py
+++ modules/env.py
@@ -28,28 +28,28 @@ class TrainingEnv(gym.Env):
 
     def __init__(self, code: str, df_tick: pd.DataFrame, render_mode=None) -> None:
         super().__init__()
+        self.CODE: str = code  # 銘柄コード
         self.df_tick: pd.DataFrame = df_tick
         self.render_mode = render_mode
 
         # ====== 報酬パラメータ ======
-        self.RATIO_PROFIT_HOLD: float = 0.01  # HOLD（建玉あり）時の含み損益からの報酬比率
+        self.RATIO_PROFIT_HOLD: float = 0.015  # HOLD（建玉あり）時の含み損益からの報酬比率
         self.COST_CONTRACT: float = 1.0  # 約定手数料（スリッページ相当）
 
-        # 報酬関連の設定
-        # エピソードにおける総報酬
-        self.pnl_total = 0
-        # 報酬保持用辞書 → 最後にデータフレーム化
-        self.dict_reward = defaultdict(list)
+        # 定数
+        self.MAX_TRADE: int = 200
 
         # インスタンス変数の初期化
-        self.code: str = code
-        self.row: int = 0
-        self.position: PositionType = PositionType.NONE
-        self.profit: float = 0.0
+        self.row: int = 0  # ティックデータの行位置
+        self.position: PositionType = PositionType.NONE  # ポジション
+        self.profit: float = 0.0  # 含み損益
+        self.n_trade: int = 0  # 約定回数
+        self.pnl_total = 0  # エピソードにおける総報酬
+        self.dict_reward = defaultdict(list)  # 報酬保持用辞書 → 最後にデータフレーム化
 
         # ポジション・マネージャ
         self.posman = posman = PositionManager()
-        posman.initPosition([self.code])
+        posman.initPosition([self.CODE])
 
         # ====== Define action_space（行動空間） ======
         n_action_space = len(ActionType)
@@ -135,17 +135,16 @@ class TrainingEnv(gym.Env):
         初期化処理
         :return:
         """
-        self.row = 0
-        self.position = PositionType.NONE
-        self.profit: float = 0.0
-        self.pnl_total: float = 0.0
-
-        # 報酬保持用辞書 → 最後にデータフレーム化
-        self.dict_reward = defaultdict(list)
+        self.row: int = 0  # ティックデータの行位置
+        self.position: PositionType = PositionType.NONE  # ポジション
+        self.profit: float = 0.0  # 含み損益
+        self.n_trade: int = 0  # 約定回数
+        self.pnl_total = 0  # エピソードにおける総報酬
+        self.dict_reward = defaultdict(list)  # 報酬保持用辞書 → 最後にデータフレーム化
 
         # ポジション・マネージャのリセットと初期化
         self.posman.reset()
-        self.posman.initPosition([self.code])
+        self.posman.initPosition([self.CODE])
 
     def reset(self, seed=None, options=None):
         """
@@ -183,7 +182,7 @@ class TrainingEnv(gym.Env):
         # データフレームからデータを一行分取得
         ts, price, ma1, diff_vwap = self.get_data(self.row)
         # 含み損益の取得
-        profit = self.posman.getProfit(self.code, price)
+        profit = self.posman.getProfit(self.CODE, price)
         # 初期報酬
         reward = 0
 
@@ -192,16 +191,18 @@ class TrainingEnv(gym.Env):
         if action_type == ActionType.BUY:
             if self.position == PositionType.NONE:
                 # 【買建】建玉がなければ買建
-                self.posman.openPosition(self.code, ts, price, action_type)
+                self.posman.openPosition(self.CODE, ts, price, action_type)
                 self.position = PositionType.LONG  # ポジションを更新
+                self.n_trade += 1  # 取引回数の更新
                 # 【報酬】
                 reward -= self.COST_CONTRACT  # 約定コスト
                 # 買建用 VWAP 判定
                 reward -= diff_vwap  # diff_vwap が負の時に買建すれば報酬
             elif self.position == PositionType.SHORT:
                 # 【返済】売建（ショート）であれば（買って）返済
-                self.posman.closePosition(self.code, ts, price)
+                self.posman.closePosition(self.CODE, ts, price)
                 self.position = PositionType.NONE  # ポジションを更新
+                self.n_trade += 1  # 取引回数の更新
                 # 【報酬】
                 reward -= self.COST_CONTRACT  # 約定コスト
                 reward += profit  # 含み損益分そっくり報酬
@@ -210,16 +211,18 @@ class TrainingEnv(gym.Env):
         elif action_type == ActionType.SELL:
             if self.position == PositionType.NONE:
                 # 【売建】建玉がなければ売建
-                self.posman.openPosition(self.code, ts, price, action_type)
+                self.posman.openPosition(self.CODE, ts, price, action_type)
                 self.position = PositionType.SHORT  # ポジションを更新
+                self.n_trade += 1  # 取引回数の更新
                 # 【報酬】
                 reward -= self.COST_CONTRACT  # 約定コスト
                 # 売建用 VWAP 判定
                 reward += diff_vwap  # diff_vwap が正の時に売建すれば報酬
             elif self.position == PositionType.LONG:
                 # 【返済】買建（ロング）であれば（売って）返済
-                self.posman.closePosition(self.code, ts, price)
+                self.posman.closePosition(self.CODE, ts, price)
                 self.position = PositionType.NONE  # ポジションを更新
+                self.n_trade += 1  # 取引回数の更新
                 # 【報酬】
                 reward -= self.COST_CONTRACT  # 約定コスト
                 reward += profit  # 含み損益分そっくり報酬
@@ -237,11 +240,13 @@ class TrainingEnv(gym.Env):
         truncated = False  # Time limit reached
         info = {}
         if len(self.df_tick) - 1 <= self.row:
-            if self.posman.hasPosition(self.code):
+            # ティックデータの末尾
+            if self.posman.hasPosition(self.CODE):
+                # 建玉があれば強制返済
+                self.posman.closePosition(self.CODE, ts, price, "強制返済")
+                self.position = PositionType.NONE  # ポジションを更新
                 reward -= self.COST_CONTRACT  # 約定コスト
                 reward += profit * (1 - self.RATIO_PROFIT_HOLD)  # 残りの含み損益分
-                self.posman.closePosition(self.code, ts, price, "強制返済")
-                self.position = PositionType.NONE  # ポジションを更新
 
             truncated = True  # ← ステップ数上限による終了
             info["done_reason"] = "truncated: last_tick"

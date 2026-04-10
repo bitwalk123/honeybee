6576e5b540a98f6c074a8001a4053b1f698bafe5
Fuhito Suguri <bitwalk@gmail.com>
2026-04-11 07:16:02 +0900

    update

--- honeybee.py
+++ honeybee.py
@@ -2,7 +2,7 @@ from modules.agent import MyPPOAgent
 
 if __name__ == "__main__":
     # 学習に使用するティックデータ
-    file_csv: str = "20260410_9984.csv"
+    list_csv: list[str] = ["20260408_9984.csv", "20260409_9984.csv", "20260410_9984.csv"]
 
     # ログフォルダ
     dir_logs = "./logs/"
@@ -14,8 +14,10 @@ if __name__ == "__main__":
     agent = MyPPOAgent(dir_logs, tb_logs)
 
     # 学習
-    n_episode = 100  # 概ねのエピソード数
-    agent.train(file_csv, n_episode)
+    n_episode = 20  # 概ねのエピソード数
+    for file_csv in list_csv:
+        agent.train(file_csv, n_episode)
 
     # 推論（現在は同じファイルで）
+    file_csv = list_csv[-1]
     agent.infer(file_csv)

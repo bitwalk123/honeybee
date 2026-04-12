ab901467972f5ef1e99ed6fa331468372f00c47c
Fuhito Suguri <bitwalk@gmail.com>
2026-04-13 08:23:56 +0900

    update

--- honeybee.py
+++ honeybee.py
@@ -22,10 +22,10 @@ if __name__ == "__main__":
     home = os.path.expanduser("~")
     path_excel = os.path.join(home, "MyProjects", "kabuto", "collection", "*.xlsx")
     list_excel_all = sorted(glob.glob(path_excel))
-    list_excel = list_excel_all[-11:-1]
+    list_excel = list_excel_all[-21:-1]
 
     # 学習
-    n_episode = 50  # 概ねのエピソード数
+    n_episode = 10  # 概ねのエピソード数
     for file_excel in list_excel:
         agent.train(file_excel, n_episode)
 

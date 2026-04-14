6df395079214624ed2e5ccf94987c84a520cf05b
Fuhito Suguri <bitwalk@gmail.com>
2026-04-15 06:51:02 +0900

    update

--- ant.py
+++ ant.py
@@ -5,29 +5,27 @@ from collections import defaultdict
 
 import pandas as pd
 
+from funcs.plot import plot_performance
 from funcs.tide import get_dt_from_excel
 from modules.agent import MyPPOAgent
 
 if __name__ == "__main__":
+    # 銘柄コード
     code = "9984"
-
+    # モデル名
     name_model = "model_test.zip"
-
     # ログフォルダ
     dir_logs = "./logs/"
-
     # TensorBoard 用ログ
     tb_logs = "./tb_logs/"
 
-    # 学習用エージェント
+    # ====== 学習用エージェント ======
     agent = MyPPOAgent(code, name_model, dir_logs, tb_logs)
-
     # 学習に使用するティックデータ
     home = os.path.expanduser("~")
     path_excel = os.path.join(home, "MyProjects", "kabuto", "collection", "*.xlsx")
     list_excel_all = sorted(glob.glob(path_excel))
     list_excel = list_excel_all[-20:]
-
     # 推論に渡す Excel リストが確かにリストになっているか確認
     if type(list_excel) is not list:
         print(f"list_excel is not list!")
@@ -38,8 +36,8 @@ if __name__ == "__main__":
         for file_excel in list_excel:
             print(file_excel)
 
-    # 推論
-    dict_perf = defaultdict(list)
+    # ====== 推論 ======
+    dict_trend = defaultdict(list)
     for file_excel in list_excel:
         dict_result = agent.infer(file_excel)
         if "transaction" in dict_result:
@@ -49,12 +47,20 @@ if __name__ == "__main__":
             n_contract = len(df)
             print(f"損益 : {pnl} 円, 約定係数 : {n_contract} 回")
 
-            dict_perf["file"].append(os.path.basename(file_excel))
-            dict_perf["code"].append(code)
-            dict_perf["pnl"].append(pnl)
-            dict_perf["contracts"].append(n_contract)
+            dict_trend["file"].append(os.path.basename(file_excel))
+            dict_trend["code"].append(code)
+            dict_trend["pnl"].append(pnl)
+            dict_trend["contracts"].append(n_contract)
 
-    df_perf = pd.DataFrame(dict_perf)
-    df_perf.index = [get_dt_from_excel(f) for f in dict_perf["file"]]
-    print(df_perf)
-    df_perf.to_pickle("performance.pkl")
+    df_trend = pd.DataFrame(dict_trend)
+    df_trend.index = [get_dt_from_excel(f) for f in dict_trend["file"]]
+
+    # ====== プロット ======
+    # 念の為、銘柄コードが一つしか存在しないことを確認
+    list_code = list(set(df_trend["code"]))
+    if len(list_code) == 1:
+        code = list_code[0]
+    else:
+        raise ValueError(f"len(list_code) is not 1!")
+    # トレンド表示
+    plot_performance(code, df_trend)
--- funcs/plot.py
+++ funcs/plot.py
@@ -1,5 +1,10 @@
 import pandas as pd
-from matplotlib import pyplot as plt
+from matplotlib import (
+    dates as mdates,
+    font_manager as fm,
+    pyplot as plt,
+    ticker as ticker,
+)
 
 
 def learning_curve(df: pd.DataFrame, subtitle: str):
@@ -15,3 +20,42 @@ def learning_curve(df: pd.DataFrame, subtitle: str):
     plt.savefig("trend_reward.png")
     #plt.show()
     plt.close()
+
+
+def plot_performance(code: str, df: pd.DataFrame):
+    # Matplotlib の共通設定
+    FONT_PATH = "fonts/RictyDiminished-Regular.ttf"
+    fm.fontManager.addfont(FONT_PATH)
+
+    # FontPropertiesオブジェクト生成（名前の取得のため）
+    font_prop = fm.FontProperties(fname=FONT_PATH)
+    font_prop.get_name()
+
+    plt.rcParams["font.family"] = font_prop.get_name()
+
+    fig = plt.figure(figsize=(6.8, 4))
+    ax = dict()
+    n = 2
+    gs = fig.add_gridspec(
+        n, 1, wspace=0.0, hspace=0.0,
+        height_ratios=[2 if i == 0 else 1 for i in range(n)]
+    )
+    for i, axis in enumerate(gs.subplots(sharex="col")):
+        ax[i] = axis
+        ax[i].grid()
+
+    total = df["pnl"].sum()
+    days = len(df)
+    ax[0].set_title(f"推論パフォーマンス ({code}) : 総収益 {total} 円/株 in {days} 日")
+    ax[0].plot(df["pnl"], color="C0")
+    ax[0].set_ylabel("損益/株")
+    ax[0].xaxis.set_major_formatter(mdates.DateFormatter("%m/%d"))
+    ax[0].yaxis.set_major_formatter(ticker.StrMethodFormatter("{x:,.0f}"))
+
+    ax[1].plot(df["contracts"], color="C1")
+    ax[1].set_ylabel("約定回数")
+
+    plt.tight_layout()
+    output = "performance.png"
+    plt.savefig(output)
+    plt.show()
